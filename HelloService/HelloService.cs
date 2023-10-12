1502b0cc6a1d8b45fd0956f5dc65f5448377c860
CEMOMRT <142478485+CEMOMRT@users.noreply.github.com>
2023-10-12 09:59:56 +0300

    Proje dosyası ekle.

---
+++ HelloService/HelloService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloService
+{
+    // NOT: "HelloService" sınıf adını kodda ve yapılandırma dosyasında birlikte değiştirmek için "Yeniden Düzenle" menüsündeki "Yeniden Adlandır" komutunu kullanabilirsiniz.
+    public class HelloService : IHelloService
+    {
+        public string GetMassage(string name)
+        {
+            return "Hi " + name;
+        }
+    }
+}
---
+++ HelloService/IHelloService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloService
+{
+    // NOT: "IHelloService" arabirim adını kodda ve yapılandırma dosyasında birlikte değiştirmek için "Yeniden Düzenle" menüsündeki "Yeniden Adlandır" komutunu kullanabilirsiniz.
+    [ServiceContract]
+    public interface IHelloService
+    {
+        [OperationContract]//Client tarafında tüketilmeye hazır metotdu sağlayan attribute
+        string GetMassage(string name);
+    }
+}
